8c01bbdcab9c5909ce9668e84643a43cf1748b98
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-02-20 21:13:48 +0100

    finished putting the sequelize commands into the index model page

--- models/index.js
+++ models/index.js
@@ -28,3 +28,16 @@ sequelize.authenticate()
 })
 
 const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.products = require ('./productModel.js')(sequelize , DataTypes)
+db.reviews = require ('./reviewModel.js')(sequelize , DataTypes)
+
+db.sequelize.sync({ force: false })
+.then(()=>{
+    console.log('yes re-sync done!')
+})
+
+module.exports = db
